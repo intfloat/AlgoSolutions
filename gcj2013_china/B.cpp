423c53480de816431121924848729f0a2109e5c8
intfloat <beidawl1992@126.com>
2013-09-14 13:09:45 +0800

    Create B.cpp

---
+++ gcj2013_china/B.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <list>
+#include <limits.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(){	
+	int t;
+	double pi = 3.141592653;
+	cin>>t;
+	for(int i=1; i<=t; i++){
+		double v, d;
+		cin>>v>>d;
+		double res = asin(9.8*d/(v*v))/2.0;
+		res = (res/pi)*180.0;
+		cout<<"Case #"<<i<<": "<<fixed<<setprecision(7)<<res<<endl;
+	}
+	return 0;
+}

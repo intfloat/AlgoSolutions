1a2322e12c3662d15f71e35c00656170f29a61f5
wangliang <wangliangpeking@gmail.com>
2014-11-21 17:11:24 +0800

    add problem I failed to solve two years ago...

--- poj/1556.cpp
+++ poj/1556.cpp
@@ -1,58 +1,105 @@
-//最悲催的事情莫过于程序写完了，却发现思路一开始就错了 …… 
-#include<iostream>
-#include<cmath>
-#include<iomanip>
+#include <vector>
+#include <list>
+#include <limits.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string.h>
+#include <stdlib.h>
+#include <cassert>
 using namespace std;
 
-const int MAXIMUM=999999;
-
-struct Wall{
-	double x;
-	double y[5];
-	double minDis[5];
+struct Point {
+	double x, y;
+	Point(double _x, double _y): x(_x), y(_y) {}
+	Point() {}
 };
-Wall wall[20];
 
-double dis(double x1,double y1,double x2,double y2);
+struct Line {
+	Point p1, p2;
+	Line(Point _p1, Point _p2): p1(_p1), p2(_p2) {}
+	Line() {}
+};
 
-int main(){
-	int t;
-		
-	while(cin>>t&&t!=-1){
-		for(int i=0;i<t;i++){
-			cin>>wall[i].x;
-			for(int j=0;j<4;j++)
-				cin>>wall[i].y[j];
-			for(int j=0;j<5;j++)			
-				wall[i].minDis[j]=MAXIMUM;
-		}
-		if(t==0){
-			cout<<"10.00"<<endl;
-			continue;
-		}
-		for(int i=0;i<t;i++){
-			for(int j=0;j<4;j++){
-				if(i==0){
-					wall[i].minDis[j]=dis(0,5,wall[i].x,wall[i].y[j]);
-					continue;
-				}
-				for(int k=0;k<4;k++){
-					wall[i].minDis[j]=min(wall[i].minDis[j],
-								wall[i-1].minDis[k]+dis(wall[i-1].x,wall[i-1].y[k],wall[i].x,wall[i].y[j]));
-				}
-			}//end inner for loop
-		}//end external for loop
-		double goal=MAXIMUM;
-		for(int i=0;i<4;i++){
-			goal=min(goal,dis(10,5,wall[t-1].x,wall[t-1].y[i]));
-		}
-		cout<<fixed<<setprecision(2)<<goal<<endl;
-	}
-	system("pause");
-	return 0;
+double cross(Point a, Point b1, Point b2) {
+	return (b1.x - a.x) * (b2.y - a.y) - (b1.y - a.y) * (b2.x - a.x);
 }
 
-//calculate distance between two points
-double dis(double x1,double y1,double x2,double y2){
-	return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+bool intersect(Line a, Line b) {
+	Point pa1 = a.p1, pa2 = a.p2;
+	Point pb1 = b.p1, pb2 = b.p2;
+	return max(pa1.x, pa2.x) > min(pb1.x, pb2.x)
+		&& max(pb1.x, pb2.x) > min(pa1.x, pa2.x)
+		&& max(pa1.y, pa2.y) > min(pb1.y, pb2.y)
+		&& max(pb1.y, pb2.y) > min(pa1.y, pa2.y)
+		&& cross(pa1, pa2, pb1) * cross(pa1, pa2, pb2) < 0
+		&& cross(pb1, pb2, pa1) * cross(pb1, pb2, pa2) < 0;	
+}
+
+double euclidean(Point p1, Point p2) {
+	return sqrt((p1.x - p2.x) * (p1.x - p2.x) 
+		        + (p1.y - p2.y) * (p1.y - p2.y));
+}
+
+vector<Line> v;
+vector<Point> ps;
+const int MAX_N = 80;
+double dis[MAX_N][MAX_N];
+int main() {
+	int N, M;
+	while (cin >> N && N >= 0) {
+		v.clear(); ps.clear();
+		ps.push_back(Point(0, 5));
+		ps.push_back(Point(10, 5));
+		double x, y;
+		Point p[4];
+		for (int i = 0; i < N; ++i) {
+			cin >> x;
+			for (int j = 0; j < 4; ++j) { cin >> y; p[j] = Point(x, y); ps.push_back(Point(x, y)); }
+			v.push_back(Line(Point(x, 0), p[0]));
+			v.push_back(Line(p[1], p[2]));
+			v.push_back(Line(p[3], Point(x, 10)));
+		}
+		int T = ps.size();
+		for (int i = 0; i < T; ++i)
+		for (int j = 0; j < T; ++j) {
+			if (i == j) dis[i][j] = 0;
+			else dis[i][j] = INT_MAX;
+		}
+		for (int i = 0; i < T; ++i)
+		for (int j = 0; j < i; ++j) {
+			Line l(ps[i], ps[j]);
+			bool ok = true;
+			for (int k = 0; k < v.size(); ++k) {
+				if (intersect(v[k], l)) { ok = false; break; }
+			}
+			if (ok) {
+				dis[i][j] = min(dis[i][j], euclidean(ps[i], ps[j]));
+				dis[j][i] = min(dis[j][i], euclidean(ps[i], ps[j]));
+			}
+		}
+
+		// floyd is easier to implement
+		for (int k = 0; k < T; ++k)
+		for (int i = 0; i < T; ++i)
+		for (int j = 0; j < T; ++j) {
+			dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
+		}
+		cout << fixed << setprecision(2) << dis[0][1] << endl;
+	}
+	return 0;
 }
---
+++ poj/2637.cpp
@@ -0,0 +1,98 @@
+#include <vector>
+#include <list>
+#include <limits.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string.h>
+#include <stdlib.h>
+#include <cassert>
+#define FOR(i, n) for (int i = 0; i < n; ++i)
+using namespace std;
+
+const int MAX_N = 50005;
+int N, rmq[MAX_N][20];
+vector<int> year, rain;
+
+void rmq_init() {
+    // memset(rmq, 0, sizeof(rmq));
+    FOR(i, N) rmq[i][0] = rain[i];
+    for (int i = 1; (1 << i) <= N; ++i) {
+        for (int j = 0; j + (1 << i) <= N; ++j) {
+            rmq[j][i] = max(rmq[j][i - 1], rmq[j + (1 << (i - 1))][i - 1]);
+        }
+    }
+    return;
+}
+ 
+int rmq_query(int l, int r) {
+    // cout << l << " " << r << endl;
+    // assert(l <= r);
+    if (l > r) return -1;
+    int k = 0;
+    while ((1 << (k + 1)) < (r - l + 1)) ++k;
+    return max(rmq[l][k], rmq[r - (1 << k) + 1][k]);
+}
+
+int main() {
+    int M, X, Y;
+    while (true) {
+    scanf("%d", &N);
+    year.clear(); rain.clear();
+    FOR(i, N) { int y, r; scanf("%d%d", &y, &r); year.push_back(y); rain.push_back(r); }
+    rmq_init();
+    scanf("%d", &M);
+    if (N == 0 && M == 0) break;
+    FOR(i, M) {
+        scanf("%d%d", &Y, &X);
+        int px = lower_bound(year.begin(), year.end(), X) - year.begin();
+        int py = lower_bound(year.begin(), year.end(), Y) - year.begin();
+        if (X <= Y) { cout << "true" << endl; continue; }
+        if (py >= N - 1 || px == 0) { cout << "maybe" << endl; continue; }
+        bool has_y = false;
+        if (py < year.size() && year[py] == Y) has_y = true;
+        if (px < year.size() && year[px] == X) {            
+            if (has_y) {
+                // cout << "have both: " << py << " " << px << endl;
+                int mx = rmq_query(py + 1, px - 1);
+                // cout << "query: " << py + 1 << " " << px << " " << mx << endl;
+                if (rain[px] > rain[py] || mx >= rain[px]) { cout << "false" << endl; continue; }
+                if (px - py == X - Y) { cout << "true" << endl; continue; }
+                else { cout << "maybe" << endl; continue; }
+            }
+            else {
+                int mx = rmq_query(py, px - 1);
+                if (mx >= rain[px]) { cout << "false" << endl; continue; }
+                else { cout << "maybe" << endl; continue; }
+            }
+        }
+        // value for year X is missing
+        else {
+            // px = px - 1;
+            // if (px < 0) { cout << "maybe" << endl; continue; }            
+            if (has_y) {
+                int mx = rmq_query(py + 1, px - 1);
+                if (rain[py] > mx) { cout << "maybe" << endl; continue; }
+                else { cout << "false" << endl; continue; }
+            }
+            else { cout << "maybe" << endl; continue; }
+        }
+    }
+    cout << endl;
+    }
+    return 0;
+}

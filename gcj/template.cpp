2383d21d7ba42d334c0335a2e87f3022c1ebfa86
wangliang <wangliangpeking@gmail.com>
2015-04-17 22:51:23 +0800

    Add google code jam template

---
+++ gcj/template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for (int i = 0; i < n; ++i)
+using namespace std;
+
+int main() {
+    int T;    
+    cin >> T;
+    FOR(tt, T) {
+        cout << "Case #" << tt + 1 << ": ";        
+    }
+    return 0;
+}

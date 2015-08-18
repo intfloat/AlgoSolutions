94dcd2c15daf8833be03d8202aeb509246b080a0
wangliang <wangliangpeking@gmail.com>
2015-08-18 14:16:34 +0800

    Update rmq code

--- library/data-structure/rmq.cpp
+++ library/data-structure/rmq.cpp
@@ -1,24 +1,21 @@
 #include <algorithm>
-#include <cassert>
 using namespace std;
-const int MAXN = 50005;
-int val[MAXN], dp[MAXN][20], N;
-void rmq_init() {
-    for (int i = 0; i < N; ++i)  
-        dp[i][0] = val[i];      
+const int MAX_N = 50005;
+int val[MAX_N], dp[MAX_N][20], N;
+inline void rmq_init() {
+    for (int i = 0; i < N; ++i)
+        dp[i][0] = val[i];
     for (int i = 1; (1 << i) <= N; ++i) {
-        for (int j = 0; (j + (1 << i) - 1) < N; ++j) {            
+        for (int j = 0; (j + (1 << i) - 1) < N; ++j) {
             dp[j][i] = max(dp[j][i - 1], dp[j + (1 << (i - 1))][i - 1]);
         }
     }
-    return;
 }
-int query(int left, int right) {
-    assert(left <= right);
-    int k = 0;  
-    int length = right - left + 1;  
+inline int query(int left, int right) {
+    int k = 0;
+    int length = right - left + 1;
     while ((1 << (k + 1)) < length) ++k;
-    int mx = max(dp[left][k], dp[right - (1 << k) + 1][k]);  
+    int mx = max(dp[left][k], dp[right - (1 << k) + 1][k]);
     return mx;
 }
 

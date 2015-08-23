31ef9e54bf5b39c410d1d03bff82d4cb607c5400
wangliang <wangliangpeking@gmail.com>
2015-08-23 11:55:09 +0800

    Update GCJ template code

--- gcj/template.cpp
+++ gcj/template.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 #define FOR(i, n) for (int i = 0; i < n; ++i)
 using namespace std;
-
 typedef long long ll;
+typedef pair<int, int> point;
 void solve() {
     return;
 }
-
 int main() {
     int TestCase;
     cin >> TestCase;

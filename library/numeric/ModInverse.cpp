e9e4c8c1a8f74e6eec3eae9e0163fc0e0ff51d30
wangliang <wangliangpeking@gmail.com>
2015-09-07 19:25:11 +0800

    Add fast_power & inverse

--- library/numeric/ModInverse.cpp
+++ library/numeric/ModInverse.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
-#include <algorithm>
-#include <stdio.h>
+#include <bits/stdc++.h>
 using namespace std;
-
+typedef long long ll;
 // a * x + b * y = gcd(a, b), return gcd(a, b)
 // answers will be stored in variables x and y
-long long extGcd(long long a, long long b, long long& x, long long& y) {
+ll extGcd(ll a, ll b, ll& x, ll& y) {
   if (b == 0) {
     x = 1;
     y = 0;
@@ -16,27 +14,26 @@ long long extGcd(long long a, long long b, long long& x, long long& y) {
     return g;
   }
 }
-
 // ASSUME: gcd(a, m) == 1, m is greater than a
 // return minimal positive a^(-1) (mod m)
-long long modInv(long long a, long long m) {
-  long long x, y;
+ll modInv(ll a, ll m) {
+  ll x, y;
   extGcd(a, m, x, y);
   return (x % m + m) % m;
 }
+const ll MOD = (ll)(1e9 + 7);
+ll fast_power(ll num, int p) {
+  if (p == 0) return 1ll;
+  if (p == 1) return num % MOD;
+  ll ret = fast_power(num, p / 2);
+  ret = (ret * ret) % MOD;
+  if (p & 1) ret = (ret * num) % MOD;
+  return ret;
+}
+inline ll inv(ll num) {
+  return fast_power(num, MOD - 2);
+}
 
-int main(){
-    long long x, y;
-    long long res = extGcd(7, 23, x, y);
-    cout<<"x: "<<x<<endl;
-    cout<<"y: "<<y<<endl;
-    cout<<res<<endl;
-    cout<<"Inverse: "<<modInv(7, 23)<<endl;
-    cout<<"Q6: "<<(modInv(3, 19)*5)%19<<endl;
-    res = 0;
-    for(int i=1; i<35; i++){
-        if(__gcd(i, 35) == 1) ++res;
-    }
-    cout<<"Q7: "<<res<<endl;
+int main() {
     return 0;
 }

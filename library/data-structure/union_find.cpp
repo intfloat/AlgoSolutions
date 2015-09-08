463f515470b8297c37d474bce06c0ff0a1fc328f
wangliang <wangliangpeking@gmail.com>
2015-09-08 18:35:33 +0800

    Update Union Find library code

--- library/data-structure/union_find.cpp
+++ library/data-structure/union_find.cpp
@@ -1,88 +1,36 @@
-#include <vector>
-#include <list>
-#include <limits.h>
-#include <map>
-#include <set>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <bitset>
-#include <algorithm>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <string.h>
-#include <stdlib.h>
+#include <bits/stdc++.h>
 using namespace std;
+const int MAX_N = 1005;
+int parent[MAX_N], sz[MAX_N];
+int N, numberOfSet;
 
-// MAXN is the number of possible largest elements
-const int MAXN = 1005;
-int parent[MAXN], rank[MAXN];
-int numberOfSet;
-// N is the number of elements
-int N;
-
-// initialize the data structure, N disjoint set 0..N-1
-void init(){
+inline void init() {
     numberOfSet = N;
-    for(int i=0; i<N; i++){
+    for (int i = 0; i < N; ++i) {
         parent[i] = i;
-        rank[i] = 1;
+        sz[i] = 1;
     }
 }
-
-// find root of a specific element indexed p
-int find(int p){
-    // without path compression
-    //while(p != parent[p]) p = parent[p];
-    //return p;
-    //
-    // with path compression
-    if(p == parent[p]) return p;
-    parent[p] = find(parent[p]);
+int _find(int p) {
+    if (p == parent[p]) return p;
+    parent[p] = _find(parent[p]);
     return parent[p];
 }
-
-// if two elements are in the same set
-bool connected(int p, int q){
-    return (find(p) == find(q));
-}
-
-// merge two sets
-void merge(int p, int q){
-    int rootP = find(p);
-    int rootQ = find(q);
-    if(rootP == rootQ) return;
-    if(rank[rootP] < rank[rootQ]){
+inline void merge(int p, int q) {
+    int rootP = _find(p);
+    int rootQ = _find(q);
+    if (rootP == rootQ) return;
+    if (sz[rootP] < sz[rootQ]) {
         parent[rootP] = rootQ;
-        rank[rootQ] += rank[rootP];
+        sz[rootQ] += sz[rootP];
     }
-    else{
+    else {
         parent[rootQ] = rootP;
-        rank[rootP] += rank[rootQ];
+        sz[rootP] += sz[rootQ];
     }
     --numberOfSet;
 }
 
-int main(){
-    cin>>N;
-    init();
-    for(int i=0; i<N; i++){
-        int x, y;
-        cout<<"enter two numbers to merge:";
-        cin>>x>>y;
-        merge(x, y);
-        cout<<"current parents: "<<endl;
-        for(int j=0; j<N; j++) cout<<find(j)<<" ";
-        cout<<endl;
-        cout<<"Number of disjoint sets: "<<numberOfSet<<endl;
-    }
+int main() {
     return 0;
 }

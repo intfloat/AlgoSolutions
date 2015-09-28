85685075ba83181654605cf72c80efbfb1eb103b
wangliang <wangliangpeking@gmail.com>
2015-09-28 11:35:42 +0800

    Simplify union-find code

--- library/data-structure/union_find.cpp
+++ library/data-structure/union_find.cpp
@@ -1,34 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int MAX_N = 1005;
-int parent[MAX_N], sz[MAX_N];
-int N, numberOfSet;
+int N, fa[MAX_N];
 
 inline void init() {
-    numberOfSet = N;
     for (int i = 0; i < N; ++i) {
-        parent[i] = i;
-        sz[i] = 1;
+        fa[i] = i;
     }
 }
-int _find(int p) {
-    if (p == parent[p]) return p;
-    parent[p] = _find(parent[p]);
-    return parent[p];
+int _find(int x) {
+    if (x == fa[x]) return x;
+    fa[x] = _find(fa[x]);
+    return fa[x];
 }
-inline void merge(int p, int q) {
-    int rootP = _find(p);
-    int rootQ = _find(q);
-    if (rootP == rootQ) return;
-    if (sz[rootP] < sz[rootQ]) {
-        parent[rootP] = rootQ;
-        sz[rootQ] += sz[rootP];
-    }
-    else {
-        parent[rootQ] = rootP;
-        sz[rootP] += sz[rootQ];
-    }
-    --numberOfSet;
+inline void merge(int x, int y) {
+    fa[x] = y;
 }
 
 int main() {

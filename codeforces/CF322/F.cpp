885efaf0692671478ea4c5235e15321b4d6f3992
wangliang <wangliangpeking@gmail.com>
2015-09-29 16:08:20 +0800

    Update code

--- codeforces/CF322/F.cpp
+++ codeforces/CF322/F.cpp
@@ -1,70 +1,54 @@
-// by fwm94
 #include <bits/stdc++.h>
-#define maxn 5009
+#define FOR(i, n) for (int i = 0; i < (int)n; ++i)
 using namespace std;
-int n,root,cnt;
-int du[maxn],sz[maxn];
-vector<int>G[maxn];
-int dp[maxn][maxn][2];
-void dfs(int u,int fa)
-{
-    sz[u]=0;
-    if((int)G[u].size()==1)
-    {
-        dp[u][0][0]=0;
-        dp[u][1][1]=0;
-        sz[u]=1;
+typedef long long ll;
+int dp[5001][2501][2];
+vector<int> g[5001];
+int leaves = 0, sz[5001];
+void solve(int pos, int fa) {
+    if (g[pos].size() == 1) {
+        dp[pos][0][0] = dp[pos][1][1] = 0;
+        sz[pos] = 1;
         return;
     }
-    dp[u][0][0]=dp[u][0][1]=0;
-    int sum=0;
-    for(int i=0;i<(int)G[u].size();i++)
-    {
-        int v=G[u][i];
-        if(v==fa) continue;
-        dfs(v,u);
-        sz[u]+=sz[v];
-        sum+=sz[v];
-        for(int j=min(cnt/2,sum);j>=0;j--)
-        {
-            dp[u][j][0]+=min(dp[v][0][0],dp[v][0][1]+1);
-            dp[u][j][1]+=min(dp[v][0][1],dp[v][0][0]+1);
-            for(int k=1;k<=sz[v]&&k<=j;k++)
-            {
-                dp[u][j][0]=min(dp[u][j][0],dp[u][j-k][0]+dp[v][k][0]);
-                dp[u][j][0]=min(dp[u][j][0],dp[u][j-k][0]+dp[v][k][1]+1);
-                dp[u][j][1]=min(dp[u][j][1],dp[u][j-k][1]+dp[v][k][1]);
-                dp[u][j][1]=min(dp[u][j][1],dp[u][j-k][1]+dp[v][k][0]+1);
+    int cnt = 0;
+    dp[pos][0][0] = dp[pos][0][1] = 0;
+    FOR(i, g[pos].size()) {
+        int to = g[pos][i];
+        if (to == fa) continue;
+        solve(to, pos);
+        cnt += sz[to];
+        for (int j = min(leaves / 2, cnt); j >= 0; --j) {
+            dp[pos][j][0] += min(dp[to][0][0], dp[to][0][1] + 1);
+            dp[pos][j][1] += min(dp[to][0][0] + 1, dp[to][0][1]);
+            for (int k = min(sz[to], j); k > 0; --k) {
+                FOR(p, 2) FOR(q, 2) {
+                    dp[pos][j][p] = min(dp[pos][j][p], dp[pos][j - k][p] + dp[to][k][q] + (p != q));
+                }
             }
         }
     }
+    sz[pos] = cnt;
 }
-int main()
-{
-    scanf("%d",&n);
-    for(int i=0;i<n-1;i++)
-    {
-        int x,y;
-        scanf("%d%d",&x,&y);
-        G[x].push_back(y);
-        G[y].push_back(x);
-        du[x]++;du[y]++;
-    }
-    if(n==2)
-    {
-        puts("1");
+int main() {
+    int n, x, y;
+    scanf("%d", &n);
+    if (n == 2) {
+        printf("1\n");
         return 0;
     }
-    cnt=0;
-    for(int i=1;i<=n;i++)
-    {
-        if(du[i]>=2)
-            root=i;
-        if(du[i]==1)
-            cnt++;
+    FOR(i, n - 1) {
+        scanf("%d %d", &x, &y);
+        g[x].push_back(y);
+        g[y].push_back(x);
     }
-    memset(dp,0x3f,sizeof(dp));
-    dfs(root,-1);
-    printf("%d\n",min(dp[root][cnt/2][0],dp[root][cnt/2][1]));
+    memset(sz, 0, sizeof sz);
+    int root = 1;
+    while (g[root].size() <= 1) ++root;
+    for (int i = 1; i <= n; ++i) leaves += (g[i].size() == 1);
+    assert(leaves % 2 == 0);
+    FOR(i, n + 1) FOR(j, leaves / 2 + 1) FOR(k, 2) dp[i][j][k] = INT_MAX / 2;
+    solve(root, -1);
+    cout << dp[root][leaves / 2][0] << endl;
     return 0;
 }

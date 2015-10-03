37df95a218d82bed1e8ee16e9abf5d6041168a1d
wangliang <wangliangpeking@gmail.com>
2015-10-03 23:09:56 +0800

    Inline functions

--- library/data-structure/bit.cpp
+++ library/data-structure/bit.cpp
@@ -4,17 +4,17 @@ const int MAX_N = 1005;
 typedef int E;
 E f[MAX_N];
 int n;
-void init() {
+inline void init() {
     memset(f, 0, sizeof f);
 }
-void update(int pos, E val) {
+inline void update(int pos, E val) {
     if (pos == 0) return;
     while (pos <= n) {
         f[pos] += val;
         pos += pos & (-pos);
     }
 }
-E query(int pos) {
+inline E query(int pos) {
     E ret = 0;
     while (pos > 0) {
         ret += f[pos];

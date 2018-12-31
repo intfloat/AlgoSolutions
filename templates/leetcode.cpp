c1baaff17425fc9257851fcd0077dce4951237e5
Liang Wang <wangliang01@fenbi.com>
2018-12-31 17:52:48 +0800

    simple template for leetcode

---
+++ templates/leetcode.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i, n) for (int i = 0; i < (int)n; ++i)
+typedef long long ll;
+typedef pair<int, int> pii;
+
+class Solution {
+
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
